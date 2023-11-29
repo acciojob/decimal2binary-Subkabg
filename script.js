9d20313f13668f8d03cf81262acb1ac48185e2cf
Subikabg <92627736+Subkabg@users.noreply.github.com>
2023-11-30 00:30:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function decimalToBinary(num) {
-  //Write you code here
-  
+  if (num === 0) {
+    return '0';
+  }
+
+  let binary = '';
+  while (num > 0) {
+    // Get the remainder when dividing by 2 (0 or 1)
+    const remainder = num % 2;
+    
+    // Add the remainder to the binary string
+    binary = remainder + binary;
+
+    // Update the number to the quotient of the division
+    num = Math.floor(num / 2);
+  }
+
+  return binary;
 }
 
+// Export the function for use in the browser
 window.decimalToBinary = decimalToBinary;
+
